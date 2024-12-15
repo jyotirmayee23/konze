1a8d9e4eb88d91b1b95c990353a5b0313f558d45
jyotirmayee <156336518+jyotirmayee23@users.noreply.github.com>
2024-12-15 15:26:08 +0530

    Update main.py

--- src/Konze/main.py
+++ src/Konze/main.py
@@ -1,18 +1,19 @@
-import boto3
-from io import BytesIO
-import json
-import datetime
-import os
-import fitz 
-from PIL import Image
-import tempfile
-from botocore.config import Config
-import requests
-import io
 
+import boto3
+import json
+import os
+import fitz
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from botocore.exceptions import ClientError
+from urllib.parse import urlparse
+import logging
+
+# Initialize boto3 clients
 lambda_client = boto3.client('lambda')
-secondary_lambda_arn = os.getenv('CHARTMATE_EMBEDDING_FUNCTION_ARN')
-print("@",secondary_lambda_arn)
+s3_client = boto3.client('s3')
+textract = boto3.client('textract')
+
+secondary_lambda_arn = os.getenv('PRIMARY_EMBEDDING_FUNCTION_ARN')
 
 def invoke_secondary_lambda_async(payload):
     response = lambda_client.invoke(
@@ -22,112 +23,204 @@ def invoke_secondary_lambda_async(payload):
     )
     return response
 
+def process_page(page_number, local_path, textract_client):
+    ("process page statrted")
+    pdf_document = fitz.open(local_path)
+    page = pdf_document.load_page(page_number)
+    pix = page.get_pixmap()
+    img_bytes = pix.tobytes("png")
+    
+    response = textract_client.detect_document_text(
+        Document={'Bytes': img_bytes}
+    )
 
-images_dir = '/tmp/images'
-supported_image_formats = ('.jpeg', '.jpg', '.png')
+    page_text = " ".join(item['Text'] for item in response['Blocks'] if item['BlockType'] == 'LINE')
 
-s3 = boto3.client('s3')
-ssm_client = boto3.client('ssm')
-textract = boto3.client('textract')
+    return page_number, page_text  # Return page number with the result
+
+def process_pdf(local_path, textract_client, job_id):
+    aggregated_text = ""
+    confidence_scores = []
+    print("local path process pdf",local_path)
+
+    with ThreadPoolExecutor() as executor:
+        pdf_document = fitz.open(local_path)
+        futures = {executor.submit(process_page, page_number, local_path, textract_client): page_number for page_number in range(len(pdf_document))}
+
+        # Collect results and sort by page number
+        results = sorted((future.result() for future in as_completed(futures)), key=lambda x: x[0])
+        for page_number, text in results:  # Unpack the sorted results
+            aggregated_text += text + " "  # Append text in sequence with a space after each block
+            
+    output_directory = f"/tmp/{job_id}_output/konzeprimary"
+    
+    text_filename = os.path.splitext(os.path.basename(local_path))[0] + '.txt'
+    text_file_path = os.path.join(output_directory, text_filename)
+
+        # Write extracted text to file
+    with open(text_file_path, 'w') as text_file:
+        text_file.write(aggregated_text)
+
+        # Upload the text file to S3
+    s3_upload_path = f"{job_id}/textfiles/primary/{text_filename}"
+    upload_file_to_s3(text_file_path, 'konze-processing-bucket', s3_upload_path)
+
+    
+    return aggregated_text
+
+def upload_file_to_s3(file_path, bucket_name, s3_path):
+    s3_client.upload_file(file_path, bucket_name, s3_path)
+    print(f"Uploaded {file_path} to s3://{bucket_name}/{s3_path}")
+
+def list_files_in_s3(bucket_name, folder_path):
+    s3_client = boto3.client('s3')
+    response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=folder_path)
+    return [obj['Key'] for obj in response.get('Contents', [])]
 
 
+def download_files_from_s3(bucket_name, folder_path, local_dir):
+    s3_client = boto3.client('s3')
+    files = list_files_in_s3(bucket_name, folder_path)
+
+    for file_key in files:
+        local_file_path = os.path.join(local_dir, os.path.relpath(file_key, folder_path))
+        local_file_dir = os.path.dirname(local_file_path)
+        
+        # Ensure the directory exists
+        if not os.path.exists(local_file_dir):
+            os.makedirs(local_file_dir)
+
+        logging.info(f"Downloading {file_key} to {local_file_path}")
+
+        try:
+            s3_client.download_file(bucket_name, file_key, local_file_path)
+            logging.info(f"Downloaded {file_key} to {local_file_path}")
+        except OSError as e:
+            logging.error(f"Failed to download {file_key} to {local_file_path}: {e}")
+            # Handle the error appropriately, e.g., retry, skip, etc.
+        except Exception as e:
+            logging.error(f"Unexpected error occurred while downloading {file_key} to {local_file_path}: {e}")
+            # Handle other potential exceptions
+
+# def download_files_from_s3(bucket_name, folder_path, local_dir):
+#     paginator = s3_client.get_paginator('list_objects_v2')
+#     print("folder path ",folder_path)
+    
+#     for page in paginator.paginate(Bucket=bucket_name, Prefix=folder_path):
+#         for obj in page.get('Contents', []):
+#             file_key = obj['Key']
+#             print("file_key",file_key)
+#             file_name = os.path.basename(file_key)
+#             print("file_name",file_name)
+#             local_file_path = os.path.join(local_dir, file_name)
+#             print("local_file_path",local_file_path)
+#             s3_client.download_file(bucket_name, file_key, local_file_path)
+#             print(f"Downloaded {file_key} to {local_file_path}")
+
+
+def list_files_in_directory(directory):
+    try:
+        files = os.listdir(directory)
+        num_files = len(files)
+        
+        if num_files == 0:
+            print(f"No files in directory: {directory}")
+        else:
+            print(f"Number of files in directory {directory}: {num_files}")
+            # print(f"Files in directory {directory}:")
+            for file in files:
+                print(file)
+    except Exception as e:
+        print(f"Error listing files in directory {directory}: {str(e)}")
+
+# def clear_directory_files(directory):
+#     if os.path.exists(directory):
+#         files = os.listdir(directory)
+#         for file in files:
+#             file_path = os.path.join(directory, file)
+#             if os.path.isfile(file_path):
+#                 os.remove(file_path)
+#                 print(f"Deleted file: {file_path}")
+#             elif os.path.isdir(file_path):
+#                 print(f"Found directory: {file_path}, consider clearing it.")
+def clear_directory_files(directory):
+    # Check if the directory exists
+    if not os.path.exists(directory):
+        print(f"Directory does not exist: {directory}")
+        return
+    
+    # Get the list of all files in the directory
+    files = os.listdir(directory)
+    
+    # If the directory is empty, return
+    if not files:
+        print(f"No files to delete in directory: {directory}")
+        return
+    
+    for file in files:
+        file_path = os.path.join(directory, file)
+        
+        # Check if it is a file and remove it
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+            print(f"Deleted file: {file_path}")
 
 def lambda_handler(event, context):
     job_id = event['job_id']
     links = event.get('links', [])
+    print("links", links)
+    parsed_url = urlparse(links)
+    bucket_name = parsed_url.netloc.split('.')[0]
+    folder_path = parsed_url.path.lstrip('/')
+
+    folder_path = os.path.join(folder_path.rstrip('/'), 'primary')
+    # folder_path = folder_path.rstrip('/')
+    print("bucket", bucket_name)
+    print("folder", folder_path)
+    
+    local_dir = f"/tmp/{job_id}/konzeprimary"
+    clear_directory_files(local_dir)
+    print("Temporary directory:", local_dir)
+    os.makedirs(local_dir, exist_ok=True)
+    
+    output_directory = f"/tmp/{job_id}_output/konzeprimary"
+    clear_directory_files(output_directory)
+    print("Output directory:", output_directory)
+    
+    os.makedirs(output_directory, exist_ok=True)
+    
+    download_files_from_s3(bucket_name, folder_path, local_dir)
+    
+    pdf_files = [
+        os.path.join(local_dir, pdf_file) 
+        for pdf_file in os.listdir(local_dir) 
+        if pdf_file.endswith('.pdf')
+    ]
+    
+    print("pdf files ", pdf_files)
+
     aggregated_text = ""
-    confidence_scores = []
+    overall_confidences = []
+    
+    for pdf_file in pdf_files:
+        try:
+            avg_confidence = process_pdf(pdf_file, textract, job_id)
+            print("text created successfully")
 
-    for link in links:
-        url_parts = link.split('/')
-        bucket_name1 = url_parts[2]
-        if '.s3.amazonaws.com' in bucket_name1:
-            bucket_name = bucket_name1.rstrip('.s3.amazonaws.com')
-        else:
-            bucket_name = bucket_name1
-
-        print("23", bucket_name)
-        object_key = '/'.join(url_parts[3:])
-                
-        if object_key.lower().endswith('.pdf'):
-            local_path = '/tmp/' + object_key.split('/')[-1]
-            s3.download_file(bucket_name, object_key, local_path)
-            base_name = os.path.splitext(object_key.split('/')[-1])[0]
-
-            pdf_document = fitz.open(local_path)
-            for page_number in range(len(pdf_document)):
-                page = pdf_document.load_page(page_number)
-                pix = page.get_pixmap()
-                output_image_path = f'/tmp/{base_name}_page_{page_number + 1}.png'
-                pix.save(output_image_path)
-
-                with open(output_image_path, 'rb') as img_file:
-                    img_bytes = img_file.read()
-                    response = textract.analyze_document(
-                        Document={'Bytes': img_bytes},
-                        FeatureTypes=["TABLES", "FORMS"]  # Include features if needed
-                    )
-
-                    total_confidence = 0
-                    block_count = 0
-                    layout_types = set()  # To collect unique layout types
-
-                    for item in response['Blocks']:
-                        if item['BlockType'] == 'LINE':
-                            aggregated_text += item['Text'] + ""
-                            total_confidence += item['Confidence']
-                            block_count += 1
-                        # Collect unique layout types (e.g., LINE, TABLE, FORM)
-                        layout_types.add(item['BlockType'])
-
-                    # Calculate average confidence if there are any blocks
-                    if block_count > 0:
-                        average_confidence = total_confidence / block_count
-                        confidence_scores.append(average_confidence)
-                        # print(f"Page number {page_number + 1} (Page {page_number + 1})")
-                        # print(f"Confidence score: {average_confidence:.2f}%")
-                        # print(f"Layout types: {', '.join(layout_types)}")
-                    else:
-                        print(f"No blocks found on page {page_number + 1}.")
-    # print("2323",confidence_scores)
-    if confidence_scores:
-        overall_average_confidence = sum(confidence_scores) / len(confidence_scores)
-        # print(f"\nOverall Average Confidence Score: {overall_average_confidence:.2f}%")
-    else:
-        print("\nNo confidence scores were calculated.")
-
-    # print("Aggregated Text:", aggregated_text.strip()) 
-
-    output_filename = f"/tmp/output_{overall_average_confidence:.2f}".replace('.', '_').lower() + ".txt"
+        except Exception as e:
+            print(f"Error processing PDF file {pdf_file}: {e}")
+            continue
 
 
-    print("Output Filename:", output_filename)
-
-    object_name = f"{job_id}/{os.path.basename(output_filename)}"
-
-    with open(output_filename, 'w') as output_file:
-        output_file.write(aggregated_text)
-    # s3.upload_file(output_filename, bucket_name, 'output_files/' + os.path.basename(output_filename))
-    # s3.upload_file(file_name, bucket_name, object_name)
-    s3.upload_file(output_filename, bucket_name, object_name)
-
-
-    # combined_result = {
-    #     "status": "Done",
-    #     "response": "completed"
-    # }
-
-    # combined_result_str = json.dumps(combined_result)
-
+    clear_directory_files(local_dir)
+    clear_directory_files(output_directory)
+    
     payload = {
         "job_id": job_id,
-        "event_data": event,
-        "links": links
+        "folder_path" : folder_path
     }
-    
-    invoke_secondary_lambda_async(payload)
 
-    # print(result)
+    invoke_secondary_lambda_async(payload)
 
     return {
         "statusCode": 200,
@@ -137,5 +230,5 @@ def lambda_handler(event, context):
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "*",
         },
-        "body": json.dumps("in progess"),
+        "body": json.dumps("in progress"),
     }
